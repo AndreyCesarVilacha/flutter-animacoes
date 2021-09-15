d1943293a109f58f701ac5c609f36e1c2665974c
Andrey Cesar Vilachã <andreycesarrodrigues@hotmail.com>
2021-09-15 13:52:06 -0300

    Iniciando projeto

---
+++ lib/AnimacaoImplicita.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AnimacaoImplicita extends StatefulWidget {
+  const AnimacaoImplicita({ Key? key }) : super(key: key);
+
+  @override
+  _AnimacaoImplicitaState createState() => _AnimacaoImplicitaState();
+}
+
+class _AnimacaoImplicitaState extends State<AnimacaoImplicita> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:animacoes/AnimacaoImplicita.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: AnimacaoImplicita(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
