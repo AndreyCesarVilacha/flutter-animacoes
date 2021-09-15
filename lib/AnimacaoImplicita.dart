075f421321bed732b129980466da414782267831
Andrey Cesar Vilachã <andreycesarrodrigues@hotmail.com>
2021-09-15 14:42:46 -0300

    Primeira animação

--- lib/AnimacaoImplicita.dart
+++ lib/AnimacaoImplicita.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
 
 class AnimacaoImplicita extends StatefulWidget {
-  const AnimacaoImplicita({ Key? key }) : super(key: key);
+  const AnimacaoImplicita({Key? key}) : super(key: key);
 
   @override
   _AnimacaoImplicitaState createState() => _AnimacaoImplicitaState();
 }
 
 class _AnimacaoImplicitaState extends State<AnimacaoImplicita> {
+
+  //baseado neste estado que ocorrera a alteração
+  bool _status = true;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnimatedContainer(
+          padding: EdgeInsets.all(20),
+          width: _status ? 200 : 300,
+          height: _status ? 300: 200,
+          color: _status ? Colors.white : Colors.purpleAccent,
+          //O tempo de duração da animação
+          duration: Duration(seconds: 1),
+          child: Image.asset("imagens/logo.png"),
+          //Como ele vai ser animado
+          curve: Curves.elasticInOut,
+        ),
+        ElevatedButton(onPressed: () {setState(() {
+          _status= !_status;
+        });}, child: Text("Alterar"))
+      ],
     );
   }
 }
